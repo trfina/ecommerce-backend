1d1f295d717bec8d69fd4959fa9574bce49fa2b3
Tammy Fina <finatammy@gmail.com>
2022-02-05 16:46:16 -0500

    added table definitions to models and key relationships for models

--- models/Tag.js
+++ models/Tag.js
@@ -6,8 +6,16 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
-  },
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    }
+  },  
   {
     sequelize,
     timestamps: false,
--- models/index.js
+++ models/index.js
@@ -4,13 +4,26 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongsTo Category, as category can have multiple products but a product can only belong to one category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
+// Products belongToMany Tags (through ProductTag).  Allows products to have multiple tags and tags to have many products.
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
