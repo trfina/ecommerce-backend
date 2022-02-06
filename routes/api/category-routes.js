7089d9c7145a935218e2f1989dae8bafa452ebaf
Tammy Fina <finatammy@gmail.com>
2022-02-05 21:24:17 -0500

    category GET finaAll and findOne complete

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,13 +4,45 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
   // be sure to include its associated Products
+  Category.findAll(
+    {
+      include: {
+        model: Product,
+        attributes: ['product_name']
+      }
+    }
+  ) 
+    .then(categoryData => res.json(categoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product,
+      attributes: ['product_name']
+    }
+  })
+  .then(categoryData => {
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id' });
+      return;
+    }
+    res.json(categoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
